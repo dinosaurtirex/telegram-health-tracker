8d8e419e4d07c47ca28d8673e38d4053bd8044fc
Kirill Kirill <sneakybeaky18@gmail.com>
2025-02-17 22:05:22 +0300

    delete js + update gitignore

--- main.js
+++
@@ -1,7 +0,0 @@
-import { getEnv } from "./utils/getEnv";
-async function main() {
-    const env = getEnv();
-    console.log(env.TelegramBotToken);
-    return;
-}
-main().catch(console.error);
--- utils/getEnv.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-export function getEnv() {
-    return process.env;
-}
