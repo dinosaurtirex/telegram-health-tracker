3838b89fea61609208b8146d12ad0eb6f85e0c45
Kirill Kirill <sneakybeaky18@gmail.com>
2025-02-26 14:47:09 +0300

    удален ебливый поганый обосанный докер

--- DockerRun.sh
+++
@@ -1,8 +0,0 @@
-cd ./database/prisma; npx prisma migrate deploy; cd ../../;
-
-docker build -t healthcare-bot .
-
-docker stop hbot 2>/dev/null || true
-docker rm hbot 2>/dev/null || true
-
-docker run -d --name hbot healthcare-bot
